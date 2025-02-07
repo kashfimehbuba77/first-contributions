7879d83cd82d10147b8c5bad258888f6fbb578df
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-02-06 19:22:32 -0500

    adding d,e,f

--- assignment1_101474002.py
+++ assignment1_101474002.py
@@ -6,6 +6,7 @@ preferred_weight_kg = 20.5  #datatype: float
 highest_reps = 25           #datatype: integer
 membership_active = True    #datatype: boolean
 
+# Dictionary storing workout stats of 4 different friends with Tuples of 3 integers
 workout_stats = {
     "Maliha": (30, 13, 20),   
     "Faiza": (25, 50, 29),  
@@ -14,3 +15,27 @@ workout_stats = {
 }
 
 print(workout_stats)
+
+for friend, minutes in list(workout_stats.items()):
+    total_minutes = sum(minutes)
+    workout_stats[f"{friend}_Total"] = total_minutes
+
+print(workout_stats)
+
+
+workout_list = [list(minutes) for minutes in workout_stats.values() 
+                if isinstance(minutes, tuple)]
+
+print(workout_list)
+
+
+workout_list = [list(minutes) for minutes in workout_stats.values() if isinstance(minutes, tuple)]
+
+yoga_running = [row[:2] for row in workout_list]
+print("Yoga & Running minutes for all friends:", yoga_running)
+
+weightlifting_last_two = [row[2] for row in workout_list[-2:]]
+print("Weightlifting minutes for last two friends:", weightlifting_last_two)
+
+
+
