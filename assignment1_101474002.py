63e54ad8259a01d035ea92dbd1811857626fc757
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-02-06 19:02:57 -0500

    adding a, b, c

---
+++ assignment1_101474002.py
@@ -0,0 +1,16 @@
+# Author: Kashfi Mehbuba
+#Assignment: #1
+
+gym_member = "Alex Alliton" #datatype: string
+preferred_weight_kg = 20.5  #datatype: float
+highest_reps = 25           #datatype: integer
+membership_active = True    #datatype: boolean
+
+workout_stats = {
+    "Maliha": (30, 13, 20),   
+    "Faiza": (25, 50, 29),  
+    "Fariha": (55, 45, 25),
+    "Tanima": (17, 26, 30)
+}
+
+print(workout_stats)
